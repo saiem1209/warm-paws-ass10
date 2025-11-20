deef50119c760a5993242bed32acb02192678000
saiem1209 <saiemhossain2001@gmail.com>
2025-11-20 20:15:35 +0600

    router setup done

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../RootLayout/RootLayout";
+import Home from "../Pages/Home";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children:[
+        {
+            index: true,
+            Component: Home,
+        },
+    ]
+  },
+]);
+
+export default router;
